c5696ff5673ee0f5a3878658f1f8aa88867b75e9
7H3-PR073C70R <okanlawondamilare24434@gmail.com>
2023-05-18 19:10:51 +0100

    show custom modal

--- lib/src/core/utils/show_custom_modal.dart
+++ lib/src/core/utils/show_custom_modal.dart
@@ -3,11 +3,15 @@ import 'package:hr56_staff/src/core/enums/enums.dart';
 import 'package:hr56_staff/src/core/extensions/extensions.dart';
 import 'package:hr56_staff/src/shared/success_modal.dart';
 
-Future<void> showInfoCustomModel(BuildContext context, UserInformation info,
-        [bool isSpouse = false,
-        String? text1,
-        String? text2,
-        String? buttonText,]) =>
+Future<void> showInfoCustomModel(
+  BuildContext context,
+  UserInformation info,
+  PageController? controller, [
+  bool isSpouse = false,
+  String? text1,
+  String? text2,
+  String? buttonText,
+]) =>
     showModalBottomSheet<Widget>(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.only(
@@ -18,7 +22,27 @@ Future<void> showInfoCustomModel(BuildContext context, UserInformation info,
       context: context,
       builder: (_) => SuccessModal(
         buttonText: buttonText ?? info.buttonText,
-        onPressed: info.onPressed(context, isSpouse),
+        onPressed: () {
+          if (controller != null) {
+            Navigator.of(context).pop();
+
+            if (isSpouse) {
+              final page = (controller.page ?? 0).toInt() + 2;
+              controller.animateToPage(
+                page,
+                duration: const Duration(milliseconds: 500),
+                curve: Curves.linear,
+              );
+            } else {
+              controller.nextPage(
+                duration: const Duration(milliseconds: 500),
+                curve: Curves.linear,
+              );
+            }
+          } else {
+            info.onPressed(context, isSpouse);
+          }
+        },
         text1: text1 ?? info.text1,
         text2: text2 ?? info.text2,
         imagePath: info.imagePath,
